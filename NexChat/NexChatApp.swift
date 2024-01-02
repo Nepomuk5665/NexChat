a54c22fcacb65b69cf78771e4689ff7bc4c0a0f2
Nepomuk Crhonek <me@nepomuk.tech>
2024-01-02 18:30:38 +0100

    Initial Commit

---
+++ NexChat/NexChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  NexChatApp.swift
+//  NexChat
+//
+//  Created by Nepomuk Crhonek on 02.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct NexChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
